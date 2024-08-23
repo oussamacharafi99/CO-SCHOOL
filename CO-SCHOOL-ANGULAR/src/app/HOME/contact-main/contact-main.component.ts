526b1c01891898dd6402891b6b7b97b725fb3b22
oussama charafi <ou.charafi@gmail.com>
2024-08-24 00:00:42 +0100

    I added the contact in the home

---
+++ CO-SCHOOL-ANGULAR/src/app/HOME/contact-main/contact-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-main',
+  templateUrl: './contact-main.component.html',
+  styleUrls: ['./contact-main.component.css']
+})
+export class ContactMainComponent {
+
+}
