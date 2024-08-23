d6327837c0395fdc5634c8f104b06537fc403eda
oussama charafi <ou.charafi@gmail.com>
2024-08-23 03:52:45 +0100

    I changed the structure of  the first page

---
+++ CO-SCHOOL-ANGULAR/src/app/HOME/header-main/header-main.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderMainComponent } from './header-main.component';
+
+describe('HeaderMainComponent', () => {
+  let component: HeaderMainComponent;
+  let fixture: ComponentFixture<HeaderMainComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderMainComponent]
+    });
+    fixture = TestBed.createComponent(HeaderMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- CO-SCHOOL-ANGULAR/src/app/app.module.ts
+++ CO-SCHOOL-ANGULAR/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './HOME/login/login.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InterceptorAuth } from './Services/interceptor.service';
+import { HomeMainComponent } from './HOME/home-main/home-main.component';
+import { HeaderMainComponent } from './HOME/header-main/header-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeMainComponent,
+    HeaderMainComponent
   ],
   imports: [
     BrowserModule,
